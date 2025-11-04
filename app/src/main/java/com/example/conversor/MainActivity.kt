e857b7d6293e691665602826a10cf9fd69eb4b6b
Paco <paco.portada@gmail.com>
2025-11-04 08:51:02 +0100

    Comprobar si está vacío el cuadro de texto

--- app/src/main/java/com/example/conversor/MainActivity.kt
+++ app/src/main/java/com/example/conversor/MainActivity.kt
@@ -10,18 +10,22 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.conversor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    val rATIO = 0.9
+    companion object {
+        const private val RATIO = 0.87
+    }
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // enableEdgeToEdge()
-        // setContentView(R.layout.activity_main)
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-        //    insets
-        //}
+        /*
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        */
         //activity_main.xml -> ActivityMainBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view: View = binding.root
@@ -31,16 +35,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View) {
+        lateinit var valor: String
+
         if (view === binding.convertir) {
             try {
                 if (binding.EurosADolares.isChecked) {
-                    binding.dolares.setText(
-                        Conversion.convertirADolares(binding.euros.text.toString(), rATIO)
-                    )
+                    valor = binding.euros.text.toString()
+                    if (!valor.isEmpty())
+                        binding.dolares.setText(Conversion.convertirADolares(valor, RATIO))
+                    else
+                            Toast.makeText(this, "Introduce un valor en los Euros", Toast.LENGTH_SHORT).show()
                 } else {
-                    binding.euros.setText(
-                        Conversion.convertirAEuros(binding.dolares.text.toString(), rATIO)
-                    )
+                    valor = binding.dolares.text.toString()
+                    if (!valor.isEmpty())
+                        binding.euros.setText(Conversion.convertirAEuros(valor, RATIO))
+                    else
+                        Toast.makeText(this, "Introduce un valor en los Dólares", Toast.LENGTH_SHORT).show()
                 }
             } catch (e: NumberFormatException) {
                 Toast.makeText(this, "Error en la conversión: " + e.message, Toast.LENGTH_SHORT).show()
